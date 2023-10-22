3b611272275cfcd16b84a9a01aa7355995e91a1a
Nat! <nat@mulle-kybernetik.com>
2023-10-22 11:37:39 +0200

    fixes for macOS warnings

---
+++ .mulle/etc/env/environment-host-ci-prerelease.sh
@@ -0,0 +1,72 @@
+#
+# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+#
+export MULLE_C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE_C11_BRANCH}
+#
+export MULLE_C11_BRANCH="prerelease"
+
+
+#
+# mulle-allocator nodetype: ${MULLE__ALLOCATOR_NODETYPE:-tar}
+#
+export MULLE__ALLOCATOR_NODETYPE="git"
+
+
+#
+# mulle-allocator url: ${MULLE__ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
+
+
+#
+# mulle-allocator branch: ${MULLE__ALLOCATOR_BRANCH}
+#
+export MULLE__ALLOCATOR_BRANCH="prerelease"
+
+
+#
+# mulle-c11 nodetype: ${MULLE__C11_NODETYPE:-tar}
+#
+export MULLE__C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE__C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE__C11_BRANCH}
+#
+export MULLE__C11_BRANCH="prerelease"
+
+
+#
+# mulle-thread nodetype: ${MULLE__THREAD_NODETYPE:-tar}
+#
+export MULLE__THREAD_NODETYPE="git"
+
+
+#
+# mulle-thread url: ${MULLE__THREAD_URL:-https://github.com/mulle-concurrent/mulle-thread/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
+
+
+#
+# mulle-thread branch: ${MULLE__THREAD_BRANCH}
+#
+export MULLE__THREAD_BRANCH="prerelease"
+
+
